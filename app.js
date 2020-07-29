8372a024e7e96bea3b7ab2eca39a42c06cb83e3a
Michael Tandy <mike.tandy.1@gmail.com>
2020-07-29 17:03:53 +0100

    sending locally stored data in response to get request in app.js. Will connect react and node apps, then move data to mongoDB.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
+// top-level/synchronous
+const blogEntries = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/blogEntries.json`)
+);
+
 app.get("/blogEntries", (req, res) => {
-  res.end("Hello port 4000");
+  res.status(200).json({
+    status: "success",
+    data: {
+      blogEntries: blogEntries,
+    },
+  });
 });
 
 const port = 4000;
