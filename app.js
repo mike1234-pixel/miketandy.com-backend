0d96e22cd6bc7e2c3fb2fab4351d3ba4bd9e231c
Michael Tandy <mike.tandy.1@gmail.com>
2020-07-29 21:37:25 +0100

    connected to mongoDB, blogEntries const is currently local data, use mongoose to use the hosted database, change blogEntries to be the hosted data.

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require("express");
 const fs = require("fs");
+var cors = require("cors");
+
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
 
 const app = express();
+app.use(cors());
+
+// dotenv.config({ path: "./config.env" });
+
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
+
+// // connection to mongodb successful, now need to read the collection and save it in blogEntries var to send the data back to react
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then((con) => {
+//     console.log(con.connections);
+//     console.log("DB connection successful.");
+//   });
 
 // top-level/synchronous
 const blogEntries = JSON.parse(
