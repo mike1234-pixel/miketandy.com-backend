09a656e4b7ee0a6503621b71fe0ba75e36b3c715
Michael Tandy <mike.tandy.1@gmail.com>
2020-08-01 16:56:57 +0100

    used OAuth2.0 and nodemailer to email myself whatever is submitted to the contact form in a post request.

--- app.js
+++ app.js
@@ -1,30 +1,83 @@
-// CREATE A CONTACT PAGE AND HANDLE POST REQUESTS
-// can just mailto from the server or something...
-
 const express = require("express");
 const fs = require("fs");
 const bodyParser = require("body-parser");
-var cors = require("cors");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post("/contact", (req, res) => {
-  // let contactFormPost = {
-  //   firstName: req.body.firstName,
-  //   lastName: req.body.lastName,
-  //   email: req.body.email,
-  //   message: req.body.email,
-  // };
+// google-OAuth
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
-  console.log("POST REQUEST OCCURRED");
-  console.log(req.body);
+// top-level/synchronous
+const blogEntries = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/blogEntries.json`)
+);
+
+app.post("/contact", (req, res) => {
+  // OAUTH
+  const myOAuth2Client = new OAuth2(
+    process.env.GOOGLE_OAUTH_CLIENT_ID,
+    process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  myOAuth2Client.setCredentials({
+    refresh_token: process.env.GOOGLE_OAUTH_REFRESH_TOKEN,
+  });
+
+  const myAccessToken = myOAuth2Client.getAccessToken();
+
+  // NODEMAILER
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.MY_EMAIL_ADDRESS,
+      clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_OAUTH_REFRESH_TOKEN,
+      accessToken: myAccessToken,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.MY_EMAIL_ADDRESS,
+    to: process.env.MY_EMAIL_ADDRESS,
+    subject: "Contact Form Message",
+    text: `Message from email address: ${req.body.email} ${req.body.firstName} ${req.body.lastName}: ${req.body.message}.`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+
+  transporter.close();
 
   res.end("submitted");
 });
 
-// dotenv.config({ path: "./config.env" });
+app.get("/blogEntries", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      blogEntries: blogEntries,
+    },
+  });
+});
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
 
 // const DB = process.env.DATABASE.replace(
 //   "<PASSWORD>",
@@ -43,23 +96,3 @@ app.post("/contact", (req, res) => {
 //     console.log(con.connections);
 //     console.log("DB connection successful.");
 //   });
-
-// top-level/synchronous
-const blogEntries = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/blogEntries.json`)
-);
-
-app.get("/blogEntries", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      blogEntries: blogEntries,
-    },
-  });
-  console.log("GET REQUEST OCCURRED");
-});
-
-const port = 4000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
